59d00b14695bfa90be396352604b9e14139e5e2e
Guilherme Fernandes <guifhunter28@gmail.com>
2025-04-25 17:27:24 -0300

    chore: initial settings

---
+++ ConcessionariaManager.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ConcessionariaManager.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
