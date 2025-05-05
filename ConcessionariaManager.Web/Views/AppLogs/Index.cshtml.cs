a4594cde0f8209419849b0e6f4ef176fe44f2e89
Guilherme Fernandes <guifhunter28@gmail.com>
2025-05-05 15:06:46 -0300

    fix: views

---
+++ ConcessionariaManager.Web/Views/AppLogs/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ConcessionariaManager.Web.Views.Logs
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
